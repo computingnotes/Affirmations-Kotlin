c794132456f3586d01bf4a37f85be9824e7d7ded
Dev Gurung <dev@imacdev.modem>
2021-12-26 20:06:18 +1100

    M

--- app/src/main/java/dev/tutorials/affirmations/model/Affirmation.kt
+++ app/src/main/java/dev/tutorials/affirmations/model/Affirmation.kt
@@ -1,3 +1,6 @@
 package dev.tutorials.affirmations.model
 
-data class Affirmation(val stringResourceId: Int)
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(@StringRes val stringResourceId: Int, @DrawableRes val imageResouceId: Int)
